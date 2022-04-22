b24424a44ae430a9db8af474c7943fe61989fdfe
Christian Dionisio <christiandionisio9432@gmail.com>
2022-04-21 23:48:27 -0500

    socketio implementado

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
 
 // Crear el servidor de express
 const app = express();
 
+// Lectura y parseo del Body
+app.use(express.json());
+
+const server = http.createServer(app);
+
+// Iniciar servidor socket.io
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+// Test coneccion socket.io
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
 
 // Escuuchar peticiones
-app.listen(3001, () => {
+server.listen(3001, () => {
     console.log(`Servidor corriendo en puerto ${3001}`);
 });
